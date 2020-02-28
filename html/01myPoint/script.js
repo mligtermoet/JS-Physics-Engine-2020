7d019f347f2e25ccd63275a5dd2f36ff7caacdc9
djsjollema <j.sjollema@ma-web.nl>
2020-02-28 14:54:44 +0100

    01 --start

--- html/01myPoint/script.js
+++ html/01myPoint/script.js
@@ -8,3 +8,12 @@ canvas.width = width;
 canvas.height = height;
 
 // begin hier met jouw code voor deze opdracht
+
+let myPoint = new Point(500,200,20,"red");
+myPoint.draw(context);
+
+let myPoint2 = new Point(100,100,40,"green");
+myPoint2.draw(context);
+
+let myPoint3 = new Point(200,400,80,"blue");
+myPoint3.draw(context);
