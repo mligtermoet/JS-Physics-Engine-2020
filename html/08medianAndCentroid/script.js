67f66ec73f0e3c1cc6319779912be7598b6bc13c
28470 <54799107+28470@users.noreply.github.com>
2020-05-13 11:38:45 +0200

    08 Afgemaakt

--- html/08medianAndCentroid/script.js
+++ html/08medianAndCentroid/script.js
@@ -9,13 +9,14 @@ canvas.height = height;
 
 // begin hier met jouw code voor deze opdracht
 
+// create namespaces
 let A,B,C;
 let lAB,lBC,lCA;
 let MAB,MBC,MCA;
 let lAMBC,lBMCA,lCMAB;
 let s;
 
-
+// create points
 A = new Point(100,100,20,"red",true);
 B = new Point(600,150,20,"green",true);
 C = new Point(300,300,20,"blue",true);
@@ -34,41 +35,37 @@ lCMAB = new LinearFunction(2,300);
 
 function animate(){
   context.clearRect(0,0,width,height);
-  
 
+  // Calculate Avarage of the points
   MAB.x = (A.x + B.x)/2;
   MAB.y = (A.y + B.y)/2;
-
-
   MBC.x = (B.x + C.x)/2;
   MBC.y = (B.y + C.y)/2;
-
-
   MCA.x = (C.x + A.x)/2;
   MCA.y = (C.y + A.y)/2;
 
-
+  // Calculate Lines betweeen points
   lAB.slope = (B.y - A.y)/(B.x - A.x);
   lAB.intercept = B.y - B.x * lAB.slope;
-
   lBC.slope = (C.y - B.y)/(C.x - B.x);
   lBC.intercept = C.y - C.x * lBC.slope;
-
   lCA.slope = (A.y - C.y)/(A.x - C.x);
   lCA.intercept = A.y - A.x * lCA.slope;
 
+  // Calculate Lines between points and avarage points
   lAMBC.slope = (A.y - MBC.y)/(A.x - MBC.x);
   lAMBC.intercept = A.y - A.x * lAMBC.slope;
-
   lBMCA.slope = (B.y - MCA.y)/(B.x - MCA.x);
   lBMCA.intercept = B.y - B.x * lBMCA.slope;
-
   lCMAB.slope = (C.y - MAB.y)/(C.x - MAB.x);
   lCMAB.intercept = C.y - C.x * lCMAB.slope;
 
+  // Calculate Centroid
   S.x = lAMBC.intersection(lBMCA).x;
   S.y = lAMBC.intersection(lBMCA).y;
 
+
+  // Draw everything
   lAB.draw(context,"black");
   lBC.draw(context,"black");
   lCA.draw(context,"black");
